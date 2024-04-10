d36843220570c4831c6e5e0ec3e31e16475f11ff
akakou <akakou571@gmail.com>
2024-04-10 09:09:39 +0900

    test: extract fs

---
+++ extract_test.go
@@ -0,0 +1,34 @@
+package extractembed
+
+import (
+	"embed"
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+//go:embed test1/*.txt test1/test2/a.txt
+var embedFiles embed.FS
+
+func TestExtract(t *testing.T) {
+	err := Extract("tmp/test-extract", &embedFiles)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	output, err := exec.Command("find", "tmp/test-extract").Output()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	excepted := `tmp/test-extract
+tmp/test-extract/test1
+tmp/test-extract/test1/b.txt
+tmp/test-extract/test1/test2
+tmp/test-extract/test1/test2/a.txt
+tmp/test-extract/test1/a.txt
+`
+
+	assert.Equal(t, excepted, string(output))
+}
